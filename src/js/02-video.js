30a4a4da379e7e5fd7f0c20a3e0e85b524d70a4d
AnnaMartsinovska <annmartsinovska@gmail.com>
2023-09-26 18:33:09 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const key = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+
+const player = new VimeoPlayer(iframe);
+
+function onPlay(currentTime) {
+    const time = currentTime.seconds;
+    localStorage.setItem(key, JSON.stringify(time));
+ };
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(key) || 0));
+
